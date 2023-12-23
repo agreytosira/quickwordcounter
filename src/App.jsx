505d07ff5cef468bf8bddf592b4691d6e9552461
Agrey Tosira <agreytosira@gmail.com>
2023-12-23 12:41:13 +0700

    style: finishing styling for responsive

--- src/App.jsx
+++ src/App.jsx
@@ -26,24 +26,30 @@ function App() {
     <>
       <main className='min-h-screen flex items-center justify-center'>
         <div className='container'>
-          <section className='wordcounter w-2/3 mx-auto'>
-            <div className='my-4 flex item-center justify-between items-center'>
-              <div className='flex items-center gap-4'>
-                <div className='flex flex-col items-start bg-indigo-300 px-4 py-2 rounded-lg text-indigo-900'>
+          <section className='wordcounter md:w-2/3 mx-auto'>
+            <div className='my-4 flex item-center justify-between items-center flex-col-reverse md:flex-row'>
+              <div className='flex items-center gap-4 w-full'>
+                <div className='w-full md:w-auto flex flex-col items-start text-left bg-indigo-300 px-4 py-2 rounded-lg text-indigo-900'>
                   <p>Total Word</p>
                   <h5 className='text-3xl font-bold'>{wordCount}</h5>
                 </div>
-                <div className='flex flex-col items-start bg-lime-300 px-4 py-2 rounded-lg text-lime-900'>
+                <div className='w-full md:w-auto flex flex-col items-start text-left bg-lime-300 px-4 py-2 rounded-lg text-lime-900'>
                   <p>Total Letter</p>
                   <h5 className='text-3xl font-bold'>{letterCount}</h5>
                 </div>
               </div>
-              <div className='flex flex-col items-end gap-0'>
-                <h1 className='text-4xl font-bold mb-1'>Quick Word Counter</h1>
+              <div className='flex items-center md:flex-col md:items-end gap-2 md:gap-0 mb-4 md:md-0'>
+                <h1 className='text-4xl font-bold mb-1 text-right'>Quick Word Counter</h1>
                 <p className='text-slate-600'>v1.0.0</p>
               </div>
             </div>
-            <textarea className='focus:ring-red-600 p-4 border w-full h-[300px] border-slate-300 rounded-lg' placeholder='Input your text here...' name='input-text' id='input-text' value={text} onChange={handleTextChange}></textarea>
+            <textarea
+              className='transition-all duration-100 ease focus:ring focus:ring-red-300 focus:outline focus:outline-red-600 p-4 border w-full h-[300px] border-slate-300 rounded-lg'
+              placeholder='Input your text here...'
+              name='input-text'
+              id='input-text'
+              value={text}
+              onChange={handleTextChange}></textarea>
             <p className='my-4'>
               Developed by <strong>Agrey Tosira</strong> - All Rights Reserved &copy; {currentYear}
             </p>

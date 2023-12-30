bfc5d0969d12c0089cf9aa98f27b8af9f3e279dc
Agrey Tosira <agreytosira@gmail.com>
2023-12-30 23:44:11 +0700

    style: move clear text button into footer

--- src/App.jsx
+++ src/App.jsx
@@ -49,25 +49,26 @@ function App() {
                 <p className='text-slate-600'>v1.0.0</p>
               </div>
             </div>
-            <div className='relative'>
-              <textarea
-                className='z-[1] relative transition-all duration-100 ease focus:ring focus:ring-indigo-300 focus:outline focus:outline-indigo-600 p-4 border w-full h-[300px] border-slate-300 rounded-lg'
-                placeholder='Input your text here...'
-                name='input-text'
-                id='input-text'
-                value={text}
-                onChange={handleTextChange}></textarea>
-              <button className='px-4 py-2 rounded-[4px] text-slate-100 bg-indigo-600 hover:bg-indigo-700 transition-all duration-300 ease font-semibold absolute right-2 bottom-4 z-50' onClick={handleClear}>
+            <textarea
+              className='z-[1] relative transition-all duration-100 ease focus:ring focus:ring-indigo-300 focus:outline focus:outline-indigo-600 p-4 border w-full h-[300px] border-slate-300 rounded-lg'
+              placeholder='Input your text here...'
+              name='input-text'
+              id='input-text'
+              value={text}
+              onChange={handleTextChange}></textarea>
+            <div className='flex justify-between items-center flex-col-reverse mt-4 lg:mt-0 lg:flex-row'>
+              <p className='my-4'>
+                Developed by{' '}
+                <strong>
+                  <a href='https://agreytosira.my.id'>Agrey Tosira</a>
+                </strong>{' '}
+                - All Rights Reserved &copy; {currentYear}
+              </p>
+
+              <button className='px-4 py-2 rounded-[4px] text-slate-100 bg-indigo-600 hover:bg-indigo-700 transition-all duration-300 ease font-semibold' onClick={handleClear}>
                 Clear Text
               </button>
             </div>
-            <p className='my-4'>
-              Developed by{' '}
-              <strong>
-                <a href='https://agreytosira.my.id'>Agrey Tosira</a>
-              </strong>{' '}
-              - All Rights Reserved &copy; {currentYear}
-            </p>
           </section>
         </div>
       </main>
